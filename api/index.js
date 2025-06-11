1c95689d3a9ad65ee666d097429211cbd3929c7a
Devika <devikasa2006@gmail.com>
2025-06-11 15:45:22 +0530

    message.

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/getData/:id',(req,res)=>{
+    console.log(req.params); 
+    
+    res.send("hello");
+});
+app.post('/createdata',(req,res)=>{
+    const data=req.body;
+    console.log(data);
+    res.send('data received');
+})
+
+
+
+
+
+
+
+
+
+
+
+const port = 3001;
+app.listen(port,()=>{
+    console.log("server running on port");
+    
+})
